f7eedf7798268cae71f29622e497294dee0536b2
Pablo CR <carp4392@gmail.com>
2021-02-16 13:58:48 -0600

    final commit

--- src/Helpers/GetData.js
+++ src/Helpers/GetData.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const API_key = '3c8f0a49a3ce49ee95b195656211202'
 
 const getData = async(lat, long) => {
-    const Url = `http://api.weatherapi.com/v1/current.json?key=${API_key}&q=${lat},${long}`
+    // const Url = `http://api.weatherapi.com/v1/current.json?key=${API_key}&q=${lat},${long}`
+    const Url = `http://api.weatherapi.com/v1/current.xml?key=${API_key}&q=${lat},${long}`
     const response = await axios(Url);
 
     return response
